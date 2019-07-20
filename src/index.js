98d1bd431371b42343eac068ef3b6945f6db9bc7
binhht7777 <binhht7777@gmail.com>
2019-07-20 15:48:02 +0700

    update0347

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-
+//Binh them thong tin
 //store
 import { createStore } from 'redux';
 import myReducer from './reducers/index';
