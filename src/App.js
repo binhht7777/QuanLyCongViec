50ee0bce1e8f8d6d94b67e0c74b97f5d14cb7ab3
binhht7777 <43498281+binhht7777@users.noreply.github.com>
2019-08-17 12:13:18 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,17 +37,7 @@ class App extends Component {
             sortBy: 'name',
             sortValue: 1
         }
-<<<<<<< HEAD
-    }
-<<<<<<< HEAD
-	constructor4444(props) { //binhpt them su kien moieee44444
-        super(props44);
-        {
-			hi lsdljsfjlkfkldjsflks
-			fasfdas
-		}
-=======
-    constructor4(props) { //binhpt them su kien moi
+	    constructor3(props) { // den con khi cung phai la dc
         super(props);
         this.state = {
             tasks: [],
@@ -61,12 +51,6 @@ class App extends Component {
             sortBy: 'name',
             sortValue: 1
         }
->>>>>>> 5550a79c982abaff7e0c95abfcbe9de30c1570e9
-    }
-    
-=======
-    }  
->>>>>>> fa84d3d86f5858332e8623e7a600fc3f500f4a2c
 
     componentWillMount() {
         if (localStorage && localStorage.getItem('keyTasks')) {
