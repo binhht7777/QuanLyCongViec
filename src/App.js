0579134a9599e0de81c2fff59acb15bee5787032
binhht7777 <43498281+binhht7777@users.noreply.github.com>
2019-08-17 11:20:41 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,6 +23,22 @@ class App extends Component {
             sortValue: 1
         }
     }
+    constructor2(props) { //binhpt them su kien moi
+        super(props);
+        this.state = {
+            tasks: [],
+            isDisplayForm: false,
+            taskEditing: null,
+            filter: {
+                name: '',
+                status: -1
+            },
+            keyWord: '',
+            sortBy: 'name',
+            sortValue: 1
+        }
+    }
+    
 
     componentWillMount() {
         if (localStorage && localStorage.getItem('keyTasks')) {
